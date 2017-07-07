5c2934787aa114ccb481765be92fccaee7e6fb5f
Aris Xanthos <aris.xanthos@unil.ch>
2017-07-07 15:53:31 +0200

    v0.8 complete with setup change

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ if sys.version_info < (3, ):
 
 from setuptools import setup, find_packages
 
-__version__ = "0.7"   # file version
+__version__ = "0.8"   # file version
 
 NAME = 'Orange3-Textable-Prototypes'
 DOCUMENTATION_NAME = 'Textable Prototypes'
 
-VERSION = '0.7'  # package version
+VERSION = '0.8'  # package version
 
 DESCRIPTION = 'Additional widgets for the Textable add-on to Orange 3.'
 LONG_DESCRIPTION = open(
