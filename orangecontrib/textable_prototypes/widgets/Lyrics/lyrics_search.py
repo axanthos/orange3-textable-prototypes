689c65e7e3e63a365476b359757aa69c4ad6c8a7
cyrille <cyrille.gay-crosier@unil.ch>
2018-03-16 17:22:34 +0100

    Ajout d'une fonction pour afficher les chansons d'un artiste spécifique
    La fonction artist_display permet d'afficher les chansons les plus populaires d'un artiste.

--- orangecontrib/textable_prototypes/widgets/Lyrics/lyrics_search.py
+++ orangecontrib/textable_prototypes/widgets/Lyrics/lyrics_search.py
@@ -16,53 +16,89 @@ result_list = {}
 ACCESS_TOKEN = "PNlSRMxGK1NqOUBelK32gLirqAtWxPzTey9pReIjzNiVKbHBrn3o59d5Zx7Yej8g"
 USER_AGENT = "CompuServe Classic/1.22"
 
+def main():
+    result_list = lyrics_search()
+    artist_display(result_list)
+
 def lyrics_search():
-    """Search a list of songs with keywords"""
+    """Searches a list of songs with keywords"""
     query_string = input("What do you want to search? ")
     page = 1
     result_number = 0
-    while page<4:
+    while page<2:
         values = {'q':query_string, 'page':page}
         data = urllib.parse.urlencode(values)
         query_url = 'http://api.genius.com/search?' + data
-        request = urllib.request.Request(query_url,headers={
-            "Authorization" : "Bearer " + ACCESS_TOKEN,
-            "User-Agent" : USER_AGENT
-            })
-        response = urllib.request.urlopen(request)
-        raw = response.read()
-        json_obj = json.loads(raw)
+        json_obj = url_request(query_url)
         body = json_obj["response"]["hits"]
 
         for result in body:
             result_number += 1
             title = result["result"]["title"]
             artist = result["result"]["primary_artist"]["name"]
+            artist_id = result["result"]["primary_artist"]["id"]
             path = result["result"]["path"]
-            result_list[result_number] = path
+            result_list[result_number] = {'artist': artist, 'artist_id':artist_id, 'path':path, 'title':title}
             print (str(result_number) + " " + title + " - " + artist)
-
         page+=1
     return result_list
 
+def artist_display(result_list):
+    """Displays the most popular songs of an artist"""
+    search_option = input("See lyrics (1) or artist's songs (2): ")
+    search_option = int(search_option)
+    if search_option == 1:
+        lyrics_display(result_list)
+    else:
+        artist_nbr = input("Enter the number of the artist: ")
+        results_per_page = input("Enter the number of results to display: ")
+        artist_nbr = int(artist_nbr)
+        artist_id = result_list[artist_nbr]['artist_id']
+        artist_url = "http://api.genius.com/artists/" + str(artist_id) + "/songs?sort=popularity&per_page=" + results_per_page
+        json_obj = url_request(artist_url)
+        body = json_obj["response"]["songs"]
+        result_number = 0
+
+        for result in body:
+            result_number += 1
+            title = result["title"]
+            artist = result["primary_artist"]["name"]
+            path = result["path"]
+            result_list[result_number] = {'artist': artist, 'path':path, 'title':title}
+            print (str(result_number) + " " + title + " - " + artist)
+
+        lyrics_display(result_list)
+
+def lyrics_display(result_list):
+    """Displays the lyrics of the song chosen by the user in the search results"""
+    song_choice = input("Enter the number of the song: ")
+    song_choice = int(song_choice)
+    result_choice = result_list[song_choice]
+    page_url = "http://genius.com" + result_choice['path']
+    lyrics = html_to_text(page_url)
+    song_info = result_choice['artist'] + " - " + result_choice['title']
+    print(song_info)
+    print(lyrics)
+
+def url_request(url):
+    """Opens a URL and returns it as a JSON object"""
+    request = urllib.request.Request(url,headers={
+            "Authorization" : "Bearer " + ACCESS_TOKEN,
+            "User-Agent" : USER_AGENT
+            })
+    response = urllib.request.urlopen(request)
+    raw = response.read()
+    json_obj = json.loads(raw)
+    return json_obj
+
 def html_to_text(page_url):
-    """Extract the lyrics (as a string) of the html page"""
+    """Extracts the lyrics (as a string) of the html page"""
     page = requests.get(page_url)
     html = BeautifulSoup(page.text, "html.parser")
     [h.extract() for h in html('script')]
     lyrics = html.find("div", class_="lyrics").get_text()
     lyrics.replace('\\n', '\n')
     return lyrics
-
-def lyrics_display(result_list):
-    """Display the lyrics of the song chosen by the user in the search results"""
-    song_choice = input("Enter the number of the song: ")
-    page_url = "http://genius.com" + result_list[int(song_choice)]
-    lyrics = html_to_text(page_url)
-    print(lyrics)
-
-def main():
-    lyrics_display(lyrics_search())
 
 if __name__ == '__main__':
     main()
