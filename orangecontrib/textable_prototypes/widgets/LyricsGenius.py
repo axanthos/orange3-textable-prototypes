1163505cafd840a387fee88c37eab896a4b3ca86
rbruni <rafael.brunibaschino@unil.ch>
2018-03-22 10:03:03 +0100

    Add LyricsGenius.py
    to be continue

---
+++ orangecontrib/textable_prototypes/widgets/LyricsGenius.py
@@ -0,0 +1,27 @@
+"""
+Class LyricsGenius
+Copyright 2018 University of Lausanne
+-----------------------------------------------------------------------------
+This file is part of the Orange3-Textable-Prototypes package.
+
+Orange3-Textable-Prototypes is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Orange3-Textable-Prototypes is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Orange-Textable-Prototypes. If not, see
+<http://www.gnu.org/licenses/>.
+"""
+
+__version__ = u"0.0.1"
+__author__ = "Cyrille Gay Crosier, Rafael Bruni Baschino, Basile Maillard"
+__maintainer__ = "Aris Xanthos"
+__email__ = "cyrille.gay-crosier@unil.ch, rafael.brunibaschino@unil.ch, basile.maillard@unil.ch"
+
+from
