4afcc28e1fff669c456bd09d7df0771d7debcf29
Maxime <maxime.bony1@gmail.com>
2018-03-22 08:52:38 +0100

    Modification du fichier py et ajout de l'icône

--- orangecontrib/textable_prototypes/widgets/LexicalHunter.py
+++ orangecontrib/textable_prototypes/widgets/LexicalHunter.py
@@ -1,50 +1,71 @@
+"""
+Class LexicalHunter
+Copyright 2017 University of Lausanne
+-----------------------------------------------------------------------------
+This file is part of the Orange3-Textable-Prototypes package.
+
+Orange3-Textable-Prototypes is free software: you can redistribute it 
+and/or modify it under the terms of the GNU General Public License as published 
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Orange3-Textable-Prototypes is distributed in the hope that it will be 
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Orange-Textable-Prototypes. If not, see 
+<http://www.gnu.org/licenses/>.
+"""
+
+__version__ = "0.1"
+__author__ = "Bony Maxime, Cappelle Simon, Pitteloud Robin"
+__maintainer__ = "Aris Xanthos"
+__email__ = "aris.xanthos@unil.ch"
+
 # Standard imports...
 from Orange.widgets import widget, gui
 from Orange.widgets.settings import Setting
 
-__version__ = "0.01"
 
-
-class LexicalHunter(widget.OWWidget):
-    """An Orange widget that allows addition, substraction and
-    multiplication of two numbers"""
+class LexicalHunter(OWTextableBaseWidget):
+    """Textable widget for identifying lexical fields in segments
+    """
 
     #----------------------------------------------------------------------
-    # Widget's metadata...
+    # Widget"s metadata...
 
-    name = "LexicalHunter"
-    description = "Search a segment for lists of words"
-    icon = "icons/mywidget.svg"
-    priority = 100
+    name = "Lexical Hunter"
+    description = "Identify words contained in lists (lexical fields)"
+    icon = "icons/lexical_hunter.svg"
+    priority = 10
 
     #----------------------------------------------------------------------
     # Channel definitions...
 
     inputs = []
     outputs = []
-
+    
     #----------------------------------------------------------------------
     # GUI layout parameters...
 
     want_main_area = False
     resizing_enabled = False
 
-    def __init__(self):
-        super().__init__()
-
-
     #----------------------------------------------------------------------
-    # User interface...
+    # Settings...
+    
+    def __init__(self):
+        """Widget creator."""
+        pass
 
-
-
-# The following code lets you execute the code outside of Orange (to view the
-# resulting interface)...
+            
 if __name__ == "__main__":
-    from PyQt4.QtGui import QApplication
     import sys
-    my_app = QApplication(list(sys.argv))
-    my_widget = LexicalHunter()
-    my_widget.show()
-    my_widget.raise_()
-    my_app.exec_()
+    from PyQt4.QtGui import QApplication
+    myApplication = QApplication(sys.argv)
+    myWidget = TheatreClassique()
+    myWidget.show()
+    myApplication.exec_()
+    myWidget.saveSettings()
