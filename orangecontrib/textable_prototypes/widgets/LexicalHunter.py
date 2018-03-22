f5905976bc5e6b70da775554dddf4b56e4fee806
SImon <simon.cappelle@unil.ch>
2018-03-22 16:54:07 +0100

    Affichage minimal
    Affichage minimal terminé et fonctionnel. Il manque plus qu'à y ajouter une fonction de traitement du segement dans huntTheLexic().
    Il faudra aussi se mettre d'accord sur comment seront définis nos champs lexicaux.

--- orangecontrib/textable_prototypes/widgets/LexicalHunter.py
+++ orangecontrib/textable_prototypes/widgets/LexicalHunter.py
@@ -4,18 +4,18 @@ Copyright 2018 University of Lausanne
 -----------------------------------------------------------------------------
 This file is part of the Orange3-Textable-Prototypes package.
 
-Orange3-Textable-Prototypes is free software: you can redistribute it 
-and/or modify it under the terms of the GNU General Public License as published 
+Orange3-Textable-Prototypes is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Orange3-Textable-Prototypes is distributed in the hope that it will be 
+Orange3-Textable-Prototypes is distributed in the hope that it will be
 useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Orange-Textable-Prototypes. If not, see 
+along with Orange-Textable-Prototypes. If not, see
 <http://www.gnu.org/licenses/>.
 """
 
@@ -27,6 +27,7 @@ __email__ = "maxime.bony@unil.ch, simon.cappelle@unil.ch, robin.pitteloud@unil.c
 from Orange.widgets import widget, gui, settings
 
 from LTTL.Segmentation import Segmentation
+from LTTL.Input import Input
 import LTTL.Segmenter as Segmenter
 
 from _textable.widgets.TextableUtils import (
@@ -35,10 +36,12 @@ from _textable.widgets.TextableUtils import (
 )
 
 # Constants...
-MIN_STEM_LEN = 3
-MAX_SUFFIX_LEN = 4
-MAX_MORPH_LEN = 50
+# Champs lexicaux #fichier texte ?
+List1={"Amour":["aimer","coeur","j'aime","embrasser"]}
+List2={"Colere":["rage","colere","sourciles froncés"]}
+List3={"Joie":["vin","biere","wisky","TICT"]}
 
+DEFAULTLIST=[List1,List2,List3]
 
 class LexicalHunter(OWTextableBaseWidget):
     """Textable widget for identifying lexical fields in segments
@@ -60,7 +63,7 @@ class LexicalHunter(OWTextableBaseWidget):
 
     #----------------------------------------------------------------------
     # Layout parameters...
-    
+
     want_main_area = False
 
     #----------------------------------------------------------------------
@@ -69,11 +72,12 @@ class LexicalHunter(OWTextableBaseWidget):
     settingsHandler = VersionedSettingsHandler(
         version=__version__.rsplit(".", 1)[0]
     )
-    
+
+    lexicalDict = settings.Setting({})
+    selectedTitles = settings.Setting([])
+    titleLabels = settings.Setting([])
     autoSend = settings.Setting(False)
-    minStemLen = settings.Setting(3)
-    maxSuffixLen = settings.Setting(4)
-    
+
     def __init__(self):
         """Widget creator."""
 
@@ -81,6 +85,8 @@ class LexicalHunter(OWTextableBaseWidget):
 
         # Other attributes...
         self.inputSeg = None
+        self.outputSeg = None
+        self.defaultDict = {}
 
         # Next two instructions are helpers from TextableUtils. Corresponding
         # interface elements are declared here and actually drawn below (at
@@ -97,91 +103,135 @@ class LexicalHunter(OWTextableBaseWidget):
         # User interface...
 
         # Options box...
-        optionsBox = gui.widgetBox(
+        titleLabelsList = gui.widgetBox(
             widget=self.controlArea,
-            box="Options",
+            box="Click to select the lexical lists",
             orientation="vertical",
         )
-        gui.spin(
-            widget=optionsBox,
+        # List of Lexical list that the user can select
+        self.titleListbox = gui.listBox(
+            widget=titleLabelsList,
             master=self,
-            value='minStemLen',
-            label='Minimum length of stems: ',
-            callback=self.sendButton.sendIf,
-            labelWidth=180,
-            tooltip=(
-                'Select the minimum number of required characters in stems'
-            ),
-            minv=MIN_STEM_LEN,
-            maxv=MAX_MORPH_LEN,
-            step=1,
+            ########## selectedTitles retourne un tabeau de int suivant la position dans selectedTitles des listes selectionnees ########
+            value="selectedTitles",    # setting (list)
+            labels="titleLabels",   # setting (list)
+            callback=self.sendButton.settingsChanged,
+            tooltip="The list of lexical list that you want to use for annotation",
         )
-        gui.separator(widget=optionsBox, height=3)
-        gui.spin(
-            widget=optionsBox,
+        self.titleListbox.setMinimumHeight(150)
+        self.titleListbox.setSelectionMode(2)
+
+        # Edit a list ...
+        self.OptionList = gui.button(
+            widget=titleLabelsList,
             master=self,
-            value='maxSuffixLen',
-            label='Maximum length of suffixes: ',
-            callback=self.sendButton.sendIf,
-            labelWidth=180,
-            tooltip=(
-                'Select the maximum possible number of characters in suffixes'
-            ),
-            minv=1,
-            maxv=MAX_SUFFIX_LEN,
-            step=1,
+            label="Edit lists",
+            callback=self.editList,
+            width=100,
         )
-        gui.separator(widget=optionsBox, height=2)
+
+        ###### START NOTA BENNE ######
+
+        # Utiliser self.labelControl.setText(str(Ma_variable)) pour afficher une variable dans le widget
+        self.labelControl = gui.widgetLabel(self.controlArea, "[J'affiche des variables pour les controler]")
+
+        #A definire plus tard
+        #gui.separator(widget=optionsBox, height=3)
+
+        ##### END NOTA BENNE ######
 
         gui.rubber(self.controlArea)
 
         # Now Info box and Send button must be drawn...
         self.sendButton.draw()
         self.infoBox.draw()
-        
+
+        self.creatDefaultDict() # Constants...
+        self.setTitleList()
         # Send data if autoSend.
         self.sendButton.sendIf()
 
+    def creatDefaultDict(self):
+        """ Creat the default dictionnaries from a list of default lexical list
+        the key must be the title of default lexical liste
+        the value is the content of the default lexical liste"""
+
+        for lexiclist in DEFAULTLIST :
+            self.defaultDict.update(lexiclist)
+
+    def setTitleList(self):
+        """Creat a list with each key of the default dictionnaries to display them on the list box
+        Be carfull, the order really metter for the selectedTitles variable !"""
+
+        self.titleLabels = self.defaultDict.keys()
+
+    def editList():
+        """ Edit the list of lexical word. Nothing to do now"""
+
     def inputData(self, newInput):
         """Process incoming data."""
+        ######### traiter inputSeg comme le segement d entree ##########
         self.inputSeg = newInput
         self.infoBox.inputChanged()
         self.sendButton.sendIf()
+        ######## pour tester ! ########
+        #self.InputSeg = "test tes2 amour et biere"
 
     def sendData(self):
         """Compute result of widget processing and send to output"""
+        #self.labelControl.setText("selectedTitles = " + str(self.selectedTitles))
 
-        # Check that there's an input...
-        if self.inputSeg is None:
-            self.send("Morphologically analyzed data", None, self)
+        # A input is needed
+        if self.inputSeg == None:
+            self.infoBox.setText(
+                "A segmentation input is needed",
+                "warning"
+            )
+            self.send("Segmentation with annotations", None, self)
             return
 
-        # For now, just send a copy of input to output (will be replaced with
-        # actual processing)...
-        self.send(
-            "Morphologically analyzed data", 
-            Segmenter.bypass(self.inputSeg, self.captionTitle),
-            self,
-        )
-        self.infoBox.setText(
-            "Actual morphological analysis not yet implemented...",
-            "error",
-        )
-        
+        # Skip if no list is selected
+        if self.titleLabels == None:
+            self.infoBox.setText(
+                "You need to define at least one lexical list",
+                "error"
+            )
+            self.send("Segmentation with annotations", None, self)
+            return
+
+        # A list must have been selected
+        if len(self.selectedTitles) == 0:
+            self.infoBox.setText(
+                "Please select one or more lexical lists.",
+                "warning"
+            )
+            self.send("Segmentation with annotations", None, self)
+            return
+
+        self.huntTheLexic()
+
         # Set status to OK and report data size...
-        # message = "%i segment@p sent to output." % len(self.segmentation)
-        # message = pluralize(message, len(self.segmentation))
-        # self.infoBox.setText(message)
-        
-        self.sendButton.resetSettingsChangedFlag()             
+        message = "%i segment@p sent to output " % len(self.outputSeg)
+        message = pluralize(message, len(self.outputSeg))
+
+        # Segmentation go to outputs...
+        self.send("Segmentation with annotations", self.outputSeg, self)
+        self.infoBox.setText(message)
+        self.sendButton.resetSettingsChangedFlag()
+
+    ######## NOTRE FONCTION PRINCIPALE !!! #######
+    def huntTheLexic(self):
+        """ Je traite le segement (inputSeg) pour en retourner un output (outputSeg) de ouf """
+        self.outputSeg = self.inputSeg #lourd!
 
     def updateGUI(self):
         """Update GUI state"""
-        pass          
+
+        if len(self.titleLabels) > 0:
+            self.selectedTitles = self.selectedTitles
 
     # The following method needs to be copied verbatim in
     # every Textable widget that sends a segmentation...
-
     def setCaption(self, title):
         if 'captionTitle' in dir(self):
             changed = title != self.captionTitle
@@ -191,13 +241,12 @@ class LexicalHunter(OWTextableBaseWidget):
         else:
             super().setCaption(title)
 
-            
+
 if __name__ == "__main__":
     import sys
     from PyQt4.QtGui import QApplication
     myApplication = QApplication(sys.argv)
-    myWidget = Linguistica()
+    myWidget = LexicalHunter()
     myWidget.show()
     myApplication.exec_()
     myWidget.saveSettings()
-
