09ee8f5dfab2e8ef0af4501b7f90b3b32d427122
dfluhman <david.fluhmann@unil.ch>
2019-04-18 09:43:00 +0200

    liaison entre les fonctions et l'interface

--- orangecontrib/textable_prototypes/widgets/MovieScripts.py
+++ orangecontrib/textable_prototypes/widgets/MovieScripts.py
@@ -36,6 +36,7 @@ import requests
 from urllib import request
 from urllib import parse
 from bs4 import BeautifulSoup
+import re
 
 from _textable.widgets.TextableUtils import (
     OWTextableBaseWidget, VersionedSettingsHandler, pluralize,
@@ -235,6 +236,61 @@ class MovieScripts(OWTextableBaseWidget):
                 self.SendButton.settingsChanged()
         else:
             super().setCaption(title)
+
+    ######################################################
+
+php_query_string = '/movie_script.php?movie='
+http_query_string = 'https://www.springfieldspringfield.co.uk/movie_scripts.php?order='
+
+title_to_href = dict()
+
+
+for lettre in ['0']:#, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+               #'N', 'O', 'P', 'K', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+    page_num = 1
+    while True:
+        page_url = http_query_string + '%s&page=%i' % (lettre, page_num)
+        page = urllib.request.urlopen(page_url)
+        soup = BeautifulSoup(page, 'html.parser')
+        script_links = soup.findAll('a', attrs={'class': re.compile("^script-list-item")})
+        if not script_links:
+            break
+        links = dict()
+        for link in soup.findAll('a', attrs={'class': re.compile("^script-list-item")}):
+            links[link.text] = link.get('href')[len(php_query_string):]
+        title_to_href.update(links)
+
+        print(page_num)
+        page_num += 1
+
+print(title_to_href)
+
+
+
+def export_scripts(title_to_href):
+    try:
+        name_file = input('Entrez le nom du fichier à exporter: ')
+        exported_file = open(name_file, 'w', encoding='utf8')
+        exported_file.write(str(title_to_href))
+        exported_file.close()
+
+        '{:*^20}'.format('title_to_href')
+
+    except IOError:
+        print('Impossible de lire le fichier')
+
+    return
+
+export_scripts(title_to_href)
+
+#This is what will get the actual script of a single movie 
+page_url = "https://www.springfieldspringfield.co.uk/movie_script.php?movie=five-minutes-of-heaven"
+page = urllib.request.urlopen(page_url)
+soup = BeautifulSoup(page, 'html.parser')
+script = soup.find("div", {"class":"movie_script"})
+print (script.text)
+
+##################################################
 
 
 if __name__ == "__main__":
