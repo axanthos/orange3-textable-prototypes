a39e6ae94bde1a4fae70468f7169e480c12bccee
dfluhman <david.fluhmann@unil.ch>
2019-04-18 10:08:08 +0200

    liaison

--- orangecontrib/textable_prototypes/widgets/MovieScripts.py
+++ orangecontrib/textable_prototypes/widgets/MovieScripts.py
@@ -239,56 +239,59 @@ class MovieScripts(OWTextableBaseWidget):
 
     ######################################################
 
-php_query_string = '/movie_script.php?movie='
-http_query_string = 'https://www.springfieldspringfield.co.uk/movie_scripts.php?order='
+    php_query_string = '/movie_script.php?movie='
+    http_query_string = 'https://www.springfieldspringfield.co.uk/movie_scripts.php?order='
 
-title_to_href = dict()
+    title_to_href = dict()
 
 
-for lettre in ['0']:#, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    for lettre in ['0']:#, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                #'N', 'O', 'P', 'K', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
-    page_num = 1
-    while True:
-        page_url = http_query_string + '%s&page=%i' % (lettre, page_num)
-        page = urllib.request.urlopen(page_url)
-        soup = BeautifulSoup(page, 'html.parser')
-        script_links = soup.findAll('a', attrs={'class': re.compile("^script-list-item")})
-        if not script_links:
-            break
-        links = dict()
-        for link in soup.findAll('a', attrs={'class': re.compile("^script-list-item")}):
-            links[link.text] = link.get('href')[len(php_query_string):]
-        title_to_href.update(links)
+        page_num = 1
+        while True:
+            page_url = http_query_string + '%s&page=%i' % (lettre, page_num)
+            page = urllib.request.urlopen(page_url)
+            soup = BeautifulSoup(page, 'html.parser')
+            script_links = soup.findAll('a', attrs={'class': re.compile("^script-list-item")})
+            if not script_links:
+                break
+            links = dict()
+            for link in soup.findAll('a', attrs={'class': re.compile("^script-list-item")}):
+                links[link.text] = link.get('href')[len(php_query_string):]
+            title_to_href.update(links)
 
-        print(page_num)
-        page_num += 1
+            print(page_num)
+            page_num += 1
 
-print(title_to_href)
+    print(title_to_href)
 
 
 
-def export_scripts(title_to_href):
-    try:
-        name_file = input('Entrez le nom du fichier à exporter: ')
-        exported_file = open(name_file, 'w', encoding='utf8')
-        exported_file.write(str(title_to_href))
-        exported_file.close()
+    def export_scripts(title_to_href):
+        try:
+            name_file = input('Entrez le nom du fichier à exporter: ')
+            exported_file = open(name_file, 'w', encoding='utf8')
+            exported_file.write(str(title_to_href))
+            exported_file.close()
 
-        '{:*^20}'.format('title_to_href')
+            '{:*^20}'.format('title_to_href')
 
-    except IOError:
-        print('Impossible de lire le fichier')
+        except IOError:
+            print('Impossible de lire le fichier')
 
-    return
+        return
 
-export_scripts(title_to_href)
+    export_scripts(title_to_href)
 
-#This is what will get the actual script of a single movie 
-page_url = "https://www.springfieldspringfield.co.uk/movie_script.php?movie=five-minutes-of-heaven"
-page = urllib.request.urlopen(page_url)
-soup = BeautifulSoup(page, 'html.parser')
-script = soup.find("div", {"class":"movie_script"})
-print (script.text)
+
+    def sendData(self):
+            """Compute result of widget processing and send to output"""
+            #This is what will get the actual script of a single movie 
+            page_url = "https://www.springfieldspringfield.co.uk/movie_script.php?movie=five-minutes-of-heaven"
+            page = urllib.request.urlopen(page_url)
+            soup = BeautifulSoup(page, 'html.parser')
+            script = soup.find("div", {"class":"movie_script"})
+            self.Outputs.sample.send(self.script)
 
 ##################################################
 
