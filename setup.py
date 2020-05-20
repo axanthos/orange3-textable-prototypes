0fbc773802bc70b6b9cde5023c43de63df6e4ec8
Aris Xanthos <aris.xanthos@unil.ch>
2020-05-20 15:02:15 +0200

    v0.23
    spacy now works on MacOS

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ if sys.version_info < (3, ):
 
 from setuptools import setup, find_packages
 
-__version__ = "0.22"   # file version
+__version__ = "0.23"   # file version
 
 NAME = 'Orange3-Textable-Prototypes'
 DOCUMENTATION_NAME = 'Textable Prototypes'
 
-VERSION = '0.22'  # package version
+VERSION = '0.23'  # package version
 
 DESCRIPTION = 'Additional widgets for the Textable add-on to Orange 3.'
 LONG_DESCRIPTION = open(
