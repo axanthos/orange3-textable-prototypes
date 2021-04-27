15b785d4eb6c04d8b59351dd12ecd1519862c8a9
Caroline Rohrbach <48179836+Crohrb@users.noreply.github.com>
2021-04-27 17:20:41 +0200

    Update MovieReviews.py

--- orangecontrib/textable_prototypes/widgets/MovieReviews.py
+++ orangecontrib/textable_prototypes/widgets/MovieReviews.py
@@ -486,11 +486,11 @@ class MovieReviews(OWTextableBaseWidget):
             )
 
         # Annotate segments...
-"""
+        """
         for idx, segment in enumerate(self.segmentation):
             segment.annotations.update(annotations[idx])
             self.segmentation[idx] = segment
-"""
+        """
 
         # Clear progress bar.
         progressBar.finish()
