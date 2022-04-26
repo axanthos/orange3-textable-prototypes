a2ca229751ee58dd65930c7956f605d607a8eaa8
Ga4646 <gavinleysin9@hotmail.com>
2022-04-26 15:12:17 +0200

    Update AudioFile_Gavin.py

--- orangecontrib/textable_prototypes/widgets/AudioFile_Gavin.py
+++ orangecontrib/textable_prototypes/widgets/AudioFile_Gavin.py
@@ -1,7 +1,7 @@
 """
 Mon fichier pour le projet 
 """
-
+import os 
 from Orange.widgets import widget, gui, settings
 from Orange.widgets.settings import Setting
 from Orange.widgets.utils.widgetpreview import WidgetPreview
@@ -174,13 +174,13 @@ class AudioFile(OWTextableBaseWidget):
             return 
 
     def browse(self):
-        exempleFichier = QFileDialog.getOpenFileName(
+        audioPath = QFileDialog.getOpenFileName(
             self,
             u'open Text File',
             self.lastLocation,
             u'Text files (*)')
-        self.file = os.path.normpath(filePath)
-        self.lastLocation = os.path.dirname(filePath)
+        self.file = os.path.normpath(audioPath)
+        self.lastLocation = os.path.dirname(audioPath)
         self.updateGUI()
         self.sendButton.settingsChanged()
 
