3c68a5e991f6bb8aaffcb2c9d5ccc7dbec2cf4ed
Zacatel <elijah.green.41@gmail.com>
2023-03-15 22:05:28 +0100

    ebauche scrapper addic7ed

--- orangecontrib/textable_prototypes/widgets/SubtitlesSeries.py
+++ orangecontrib/textable_prototypes/widgets/SubtitlesSeries.py
@@ -1,1 +1,27 @@
-'''Widget to gather subtitle files from the website addic7ed'''
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.addic7ed.com'
+
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code != 200:
+    print('Error: Could not retrieve website')
+    exit()
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find all <a> tags with the href attribute starting with 'serie/'
+serie_links = soup.find_all('a', href=lambda href: href and href.startswith('serie/'))
+
+# Check if any serie_links were found
+if not serie_links:
+    print('Error: No serie_links found')
+    exit()
+
+# Loop through each serie_link and extract the text content
+for link in serie_links:
+    print(link.get_text())
