4efaf8266d41167c81c4d47adc660adbc81221e6
NEBULA-t <61778155+NEBULA-t@users.noreply.github.com>
2023-03-23 10:39:00 +0100

    v0.0.1 Text intro

--- orangecontrib/textable_prototypes/widgets/SubtitlesSeries.py
+++ orangecontrib/textable_prototypes/widgets/SubtitlesSeries.py
@@ -1,3 +1,29 @@
+"""
+Class SubtitlesSeries
+Copyright 2022-2023 University of Lausanne
+-----------------------------------------------------------------------------
+This file is part of the Orange3-Textable-Prototypes package.
+
+Orange3-Textable-Prototypes is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Orange3-Textable-Prototypes is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Orange-Textable-Prototypes. If not, see
+<http://www.gnu.org/licenses/>.
+"""
+
+__version__ = u"0.0.1"
+__author__ = "Samantha Allendes, Antoine Vigand, Elijah Green, Thomas Rywalski"
+__maintainer__ = "Aris Xanthos"
+__email__ = "aris.xanthos@unil.ch"
+
 import requests
 from bs4 import BeautifulSoup
 
