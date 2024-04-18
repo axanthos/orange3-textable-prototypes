09ab946008d262d3bb426c5516675d828a35e016
RoseMarly <rose.marly@unil.ch>
2024-04-18 13:52:15 +0200

    Ajout des annotations de chaque post

--- nice_functions_waiting_for_module.py
+++ nice_functions_waiting_for_module.py
@@ -14,7 +14,7 @@ from LTTL.Input import Input
 
 def fetch_user_posts(username_at_instance):
     "Takes a string like ' (@)user@instance.net ' and returns a dictionnary of all posts from user"
-    #TODO fix parsing if string is "https://etc..."
+    #TODO fix parsing if string is "https://rivals.space/@macron"
     #Delete the first "@", if given
     if username_at_instance[0] == "@":
         username_at_instance = username_at_instance[1:]
@@ -24,14 +24,14 @@ def fetch_user_posts(username_at_instance):
     domain = f"https://{instance}/"
 
     #Initialize connexion to instance
-    MyMastodon = Mastodon(api_base_url=domain)
-    print(user, instance, domain, MyMastodon) #Debug
+    myMastodon = Mastodon(api_base_url=domain)
+    print(user, instance, domain, myMastodon) #Debug
 
     #Get ID from user
-    user_id = MyMastodon.account_lookup(user).id
+    user_id = myMastodon.account_lookup(user).id
     print(user_id)
 
-    all_posts = MyMastodon.account_statuses(user_id)
+    all_posts = myMastodon.account_statuses(user_id)
     return all_posts
 
 def createSegmentation(posts):
@@ -41,15 +41,40 @@ def createSegmentation(posts):
     """
     segments = []
     #TODO for later: annotate (like, username, hasPhoto...) each post (easy and useful)
-    #TODO Certains segments sont vides, probablements des rts, qui n'ont rien à afficher en .content
-    # --> les supprimer de posts (selon un filtre) ou prendre le content du post partagé
+    #TODO Certains segments sont vides, images sans texte, qui n'ont rien à afficher en .content
+    # -> Mettre une case dans le GUI pour exclure ou non les textes vides
+    # (les posts vides restent utiles pour avoir les valeurs de stats..)
+    #Annotations vides ou pas d'annotations ?
     for post in posts:
         input_seg = Input(post.content)
         str_index = input_seg[0].str_index
         segments.append(
             Segment(
                 str_index=str_index,
-                annotations={"Visibility": post.visibility},
+                annotations={
+                    "Account" : post.account.username,
+                    "AccountDisplayName" : post.account.display_name,
+                    "Date" : post.created_at, #TODO Format
+                    "URL" : post.url,
+                    "IsAReply" : bool(post.in_reply_to_id),
+                    "IsAReblog" : bool(post.reblog),
+                    "IsSensitive" : post.sensitive,
+                    "HasMedias" : bool(post.media_attachments), #Rajouter types de médias ?
+                    "HasContentWarning" : bool(post.spoiler_text),
+                    "ReblogId" : post.reblog.id if post.reblog else None,
+                    "PeopleMentionnedId" : post.mentions if post.mentions else None, #TODO id
+                    "ReplyToPostId" : post.in_reply_to_id,
+                    "ReplyToAccountId" : post.in_reply_to_account_id,
+                    "SpoilerText" : post.spoiler_text,
+                    "Visibility" : post.visibility,
+                    "Application" : post.application.get("name") if post.application else None, #TODO tester
+                    "Likes" : post.favourites_count,
+                    "Repost" : post.reblogs_count,
+                    "Language" : post.language,
+                    "Tags" : post.tags if post.tags else None, #TODO tester
+                    "Poll" : post.poll, #TODO Format (ou enlever ?)
+                    "CustomEmojis" : post.emojis if post.emojis else None, #TODO tester
+                    },
                 )
             )
     segmentation = Segmentation(segments)
