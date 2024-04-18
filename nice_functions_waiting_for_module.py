37fe35e858423fdb5c553e937df67b28b821b4bd
RoseMarly <rose.marly@unil.ch>
2024-04-18 11:55:30 +0200

    createSegmentation
    crée la segmentation des posts, avec en annotation le paramètre de visiblité (Public, dé-listé)
    
    À régler: les reposts sont pris aussi, mais sont vides (n'ont pas de .content)

--- nice_functions_waiting_for_module.py
+++ nice_functions_waiting_for_module.py
@@ -8,16 +8,17 @@ Will also be integrated
 """
 
 from mastodon import Mastodon
+from LTTL.Segment import Segment
+from LTTL.Segmentation import Segmentation
 from LTTL.Input import Input
-from LTTL.Utils import tuple_to_simple_dict, sample_dict
 
 def fetch_user_posts(username_at_instance):
-    "Takes a string like ' (@)user@instance.net ' and returns a dictionnary of all posts from the user"
+    "Takes a string like ' (@)user@instance.net ' and returns a dictionnary of all posts from user"
     #TODO fix parsing if string is "https://etc..."
     #Delete the first "@", if given
     if username_at_instance[0] == "@":
         username_at_instance = username_at_instance[1:]
-    
+
     #Get user and instance frome input, format it in domain
     user, instance = username_at_instance.split("@")
     domain = f"https://{instance}/"
@@ -25,7 +26,7 @@ def fetch_user_posts(username_at_instance):
     #Initialize connexion to instance
     MyMastodon = Mastodon(api_base_url=domain)
     print(user, instance, domain, MyMastodon) #Debug
-    
+
     #Get ID from user
     user_id = MyMastodon.account_lookup(user).id
     print(user_id)
@@ -33,11 +34,31 @@ def fetch_user_posts(username_at_instance):
     all_posts = MyMastodon.account_statuses(user_id)
     return all_posts
 
-def segmentation(posts):
-    "Takes a dictionary of posts, and create an input of each of their content. Concatenate it in a single output"
+def createSegmentation(posts):
+    """
+    Takes a dictionary of posts, and create an input (in HTML) of each of their content.
+    Concatenate it in a single output
+    """
+    segments = []
     #TODO for later: annotate (like, username, hasPhoto...) each post (easy and useful)
-    pass    
+    #TODO Certains segments sont vides, probablements des rts, qui n'ont rien à afficher en .content
+    # --> les supprimer de posts (selon un filtre) ou prendre le content du post partagé
+    for post in posts:
+        input_seg = Input(post.content)
+        str_index = input_seg[0].str_index
+        segments.append(
+            Segment(
+                str_index=str_index,
+                annotations={"Visibility": post.visibility},
+                )
+            )
+    segmentation = Segmentation(segments)
+
+    for segment in segmentation:
+        print(segment)
+        print(segment.get_content())
+    print(f"found {len(posts)} posts on this account")
 
 if __name__ == "__main__":
     posts = fetch_user_posts("@macron@rivals.space")
-    print(annotate(posts))
+    createSegmentation(posts)
