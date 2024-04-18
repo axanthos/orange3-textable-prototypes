aafbee6f1cce3e058c885266e342c8984a6c9789
RoseMarly <rose.marly@unil.ch>
2024-04-18 17:16:41 +0200

    TODO
    ajout de pistes pour la suite (fetch un nombre n de posts...)

--- nice_functions_waiting_for_module.py
+++ nice_functions_waiting_for_module.py
@@ -13,11 +13,14 @@ from LTTL.Segmentation import Segmentation
 from LTTL.Input import Input
 
 def fetchUserPosts(username_at_instance, n = 100):
-    """
-    Takes a string like ' (@)user@instance.net ' and returns a dictionnary of all posts from user
+    """Takes a string like (@)user@instance.net and returns a dictionnary of all posts from user"""
+
     #TODO loop to get n posts, instead of just one (?) request
     #TODO fix parsing if string is "https://rivals.space/@macron"
-    """
+    #TODO on peut ajouter directement ici les tris MediaOnly/ExcludeRepost/ExcludeReply cf.
+    #https://mastodonpy.readthedocs.io/en/stable/_modules/mastodon/accounts.html?highlight=account_statuses
+    #(ça sera plus efficace que trier en front)
+    #TODO voir avec max_id min_id since_id et limit pour boucler sur n posts
 
     #Delete the first "@", if given (my parsing is ugly, I (Rose) will do better later)
     if username_at_instance[0] == "@":
@@ -33,25 +36,23 @@ def fetchUserPosts(username_at_instance, n = 100):
 
     #Get ID from user
     user_id = myMastodon.account_lookup(user).id
-    print(f"@{user}'s id is: {user_id}", "\n")
+    print(f"{username_at_instance}'s id is: {user_id}", "\n")
 
-    #Get all posts (for now just 1 request, seems to be 20 posts)
-    all_posts = myMastodon.account_statuses(user_id)
+    #Get all posts (for now just 1 request, 20 or 40 if we add limit=n (strange ??))
+    all_posts = myMastodon.account_statuses(user_id, limit=n)
 
-    print(f"Got {len(all_posts)} from {username_at_instance}", "\n")
+    print(f"Got {len(all_posts)} posts from {username_at_instance}", "\n")
     return all_posts
 
 def createSegmentation(posts_dict):
-    """
-    Takes a dictionary of posts, and create an input (in HTML) of each of their content.
-    Concatenate it in a single output
+    """Takes a dictionary of posts, and create an input (in HTML) of each of their content.
+    Concatenate it in a single output"""
 
-    TODO for later: annotate (like, username, hasPhoto...) each post (easy and useful)
-    TODO Certains segments sont vides, RT et images sans texte, n'ont rien à afficher en .content
-    -> Mettre une case dans le GUI pour exclure ou non les textes vides (les posts vides
-    restent utiles pour avoir les annotations, pour les stats..)
-    Q: Mieux vaut annotations vides (None; comme actuellement) ou pas d'annotations ?
-    """
+    #TODO for later: annotate (like, username, hasPhoto...) each post (easy and useful)
+    #TODO Certains segments sont vides, RT et images sans texte, n'ont rien à afficher en .content
+    #-> Mettre une case dans le GUI pour exclure ou non les textes vides (les posts vides
+    #restent utiles pour avoir les annotations, pour les stats..)
+    #Q: Mieux vaut annotations vides (None; comme actuellement) ou pas d'annotations ?
 
     #Initialisation de notre liste de segments
     segments = []
@@ -79,7 +80,7 @@ def createSegmentation(posts_dict):
                     "HasMedias" : bool(post.media_attachments), #Rajouter types de médias ?
                     "HasContentWarning" : bool(post.spoiler_text),
                     "ReblogId" : post.reblog.id if post.reblog else None,
-                    "PeopleMentionnedId" : post.mentions if post.mentions else None, #TODO id
+                    "PeopleMentionnedId" : post.mentions if post.mentions else None, #TODO get id of accounts (or username ?)
                     "ReplyToPostId" : post.in_reply_to_id,
                     "ReplyToAccountId" : post.in_reply_to_account_id,
                     "SpoilerText" : post.spoiler_text,
@@ -105,4 +106,5 @@ def createSegmentation(posts_dict):
 
 if __name__ == "__main__":
     posts = fetchUserPosts("@macron@rivals.space")
+    #Le compte parodique de Macron a seulement 60~ posts, c'est pratique pour tester
     createSegmentation(posts)
