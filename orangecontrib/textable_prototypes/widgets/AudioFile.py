0ece8c1fa521f9f5fab33de13be73c56663fc6e1
Olivia-L-V <80695018+Olivia-L-V@users.noreply.github.com>
2024-05-23 13:46:53 +0200

    Audio File mise en page code

--- orangecontrib/textable_prototypes/widgets/AudioFile.py
+++ orangecontrib/textable_prototypes/widgets/AudioFile.py
@@ -1,6 +1,29 @@
 """
-AUDIOFILE
+Class TheatreClassique
+Copyright 2022 University of Lausanne
+-----------------------------------------------------------------------------
+This file is part of the Orange3-Textable-Prototypes package.
+
+Orange3-Textable-Prototypes is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Orange3-Textable-Prototypes is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Orange-Textable-Prototypes. If not, see
+<http://www.gnu.org/licenses/>.
 """
+
+__version__ = u"0.1.6"
+__author__ = " Gavin Vinasi, Johan Cuda, Rebecca Kneubuehler"
+__maintainer__ = "Aris Xanthos"
+__email__ = "aris.xanthos@unil.ch"
+
 import os 
 from Orange.widgets import widget, gui, settings
 from Orange.widgets.settings import Setting
