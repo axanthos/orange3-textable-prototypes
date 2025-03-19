d2c98dc89e4ca25ab2c9c0c5355706d7c94520e7
Aris Xanthos <aris.xanthos@unil.ch>
2025-03-19 21:01:55 +0100

    v0.31
    addition of demo Textable widget

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ if sys.version_info < (3, ):
 
 from setuptools import setup, find_packages
 
-__version__ = "0.30"   # file version
+__version__ = "0.31"   # file version
 
 NAME = 'Orange3-Textable-Prototypes'
 DOCUMENTATION_NAME = 'Textable Prototypes'
 
-VERSION = '0.30'  # package version
+VERSION = '0.31'  # package version
 
 DESCRIPTION = 'Additional widgets for the Textable add-on to Orange 3.'
 LONG_DESCRIPTION = open(
