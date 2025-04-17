e428fede0c7187c88d4d93b7a7c1e1de296bf8c8
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2025-04-17 11:26:24 +0200

    error message

--- orangecontrib/textable_prototypes/widgets/YouGet.py
+++ orangecontrib/textable_prototypes/widgets/YouGet.py
@@ -48,6 +48,7 @@ from youtube_comment_downloader import *
 # pour tester l'url
 import requests
 
+import re
 
 
 class YouGet(OWTextableBaseWidget):
@@ -205,6 +206,12 @@ class YouGet(OWTextableBaseWidget):
             # cannot operate properly at this point.
             self.send("New segmentation", None)
             return
+        """ if self.url == "bonjour": """
+        if not re.match(r"^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$", self.url):
+            self.infoBox.setText("Please only add YouTube URLs.", "error")
+            self.send("New segmentation", None)
+            return
+            "https://chatgpt.com/share/6800c404-cb74-8000-afef-e321b9517c47"
 
         # If the widget creates new LTTL.Input objects (i.e.
         # if it imports new strings in Textable), make sure to
